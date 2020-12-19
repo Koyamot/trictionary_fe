9ab70c281b75ff923cc0db06505b3fed4b9f9383
christiano39 <carneson39@gmail.com>
2020-12-18 18:02:32 -0700

    keep track of players definition submissions

--- src/components/game/Lobby.js
+++ src/components/game/Lobby.js
@@ -1,6 +1,24 @@
+import { useState } from "react";
+
 const Lobby = (props) => {
   const { gameState, socket } = props;
+  const [definition, setDefinition] = useState("");
   const isHost = gameState.host.id === socket.id;
+
+  const handleDefinitionChange = (e) => {
+    const { value } = e.target;
+    setDefinition(value);
+  };
+
+  const handleStartGame = (e) => {
+    e.preventDefault();
+    socket.emit("start game", gameState.lobbyCode);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    socket.emit("definition submitted", definition, gameState.lobbyCode);
+  };
 
   return (
     <div className="lobby">
@@ -10,17 +28,40 @@ const Lobby = (props) => {
         <ul className="players-list">
           {gameState.players.map((player) => {
             return (
-              <li key={player.id} className="player">
+              <li
+                key={player.id}
+                className={
+                  player.definition
+                    ? "player submitted"
+                    : "player not-submitted"
+                }
+              >
                 {player.username}
               </li>
             );
           })}
         </ul>
       </div>
-      {isHost ? (
-        <button>Start Game</button>
+      {isHost && !gameState.started ? (
+        <button onClick={handleStartGame}>Start Game</button>
       ) : (
-        <p>Waiting for host to start game...</p>
+        !gameState.started && <p>Waiting for host to start game...</p>
+      )}
+      {gameState && gameState.started && (
+        <>
+          <div className="word-container">
+            <h2 className="word">{gameState.word}</h2>
+          </div>
+          <form onSubmit={handleSubmit}>
+            <textarea
+              name="definition"
+              placeholder="Enter your definition here"
+              value={definition}
+              onChange={handleDefinitionChange}
+            />
+            <button>Submit</button>
+          </form>
+        </>
       )}
     </div>
   );
